9139359e1bee933ac8ce1bebde28dd47ff97e9fb
abhi <69461060+a-kun-alt@users.noreply.github.com>
2020-08-10 12:17:06 +0100

    Add files via upload

---
+++ coffee_Machine_Stage_1.java
@@ -0,0 +1,19 @@
+package com.AbhinavMaddukuri;
+
+public class coffee_Machine_Stage_1
+{
+    public static void main(String[] args)
+        {
+            // Use system out print ln for several basic statements //
+            System.out.println(
+            "Starting to make a coffee\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready!");
+        }
+}
+
+
